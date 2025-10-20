77c7650eae1ae9b97a8ff0b45fd8371e6d1956a9
opzc35 <opzc35@gmail.com>
2025-10-20 22:17:49 +0800

    将题目列表单独成为一个文件，便于管理

--- index.py
+++ index.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
+import json
 
-problems = { "P1001": {"name":"A+B Problem", "id":"P1001", "describe": u"给你两个整数，请你输出他们的和。", "inputformat": u"一行两个整数a和b", "outputformat": u"输出他们的和", "samples": [["1 1", "2"], ["2 2", 4]]}}
+try:
+    with open('problems.json', 'r', encoding='utf-8') as f:
+        problems = json.load(f)
+except:
+    problems = {}
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 @app.route('/problem/<pid>')
 def problem(pid):
     if pid == 'list':
         return render_template('problemlist.html', problems=problems)
     return render_template('watchproblem.html', problem=problems[pid])
+
 if __name__ == '__main__':
     app.run(debug=True)
