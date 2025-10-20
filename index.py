1abdb161c26d7b87e614b6d5cb5a35a9d7628944
opzc35 <opzc35@gmail.com>
2025-10-20 21:59:16 +0800

    更新题目列表功能，方便查看题目列表

--- index.py
+++ index.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/watch/<pid>')
-def watch(pid):
+@app.route('/problem/<pid>')
+def problem(pid):
+    if pid == 'list':
+        return render_template('problemlist.html', problems=problems)
     return render_template('watchproblem.html', problem=problems[pid])
-
 if __name__ == '__main__':
     app.run(debug=True)
